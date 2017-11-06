48cfd6e82d4ce970cb586b9acaa89caac3cd1003
w-m-d <matthew.d.weger@gmail.com>
2017-11-06 09:13:46 -0500

    updating readme and adding -y to apt install

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # neovimsetup
-# neovimsetup
+I'm switching to neovim so i decided to make it lightweight this is a script that installs everything and sets up neovim with my plugins using vim-plug.
+
--- install_vim.sh
+++ install_vim.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo apt update
-sudo apt install neovim
+sudo apt install neovim -y
 
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
